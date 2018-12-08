436ca21626a568856649963b003bad59f5b9cb36
kjnam <kyujnam@gmail.com>
2018-12-08 22:33:24 +0900

    test: Web에 한정 된 슬라이스 테스트에서 SpringBootTest로 변경
    - Repository Mocking 등의 부담으로 ㅣㄴ함

--- src/test/java/io/iamkyu/controller/EventControllerTest.java
+++ src/test/java/io/iamkyu/controller/EventControllerTest.java
@@ -1,6 +1,5 @@
 package io.iamkyu.controller;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.hateoas.MediaTypes.HAL_JSON_UTF8_VALUE;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpHeaders.LOCATION;
@@ -12,12 +11,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.iamkyu.domain.Event;
-import io.iamkyu.domain.EventRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -26,7 +24,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTest {
 
     @Autowired
@@ -34,9 +33,6 @@ public class EventControllerTest {
 
     @Autowired
     private ObjectMapper mapper;
-
-    @MockBean
-    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
@@ -51,8 +47,6 @@ public class EventControllerTest {
                 .limitOfEnrollment(100)
                 .location("서울특별시")
                 .build();
-        event.setId(100);
-        when(eventRepository.save(event)).thenReturn(event);
 
         //when then
         mockMvc.perform(post("/api/events")
