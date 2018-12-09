1e633e58fd5d152317516b8a110029a5bab1a7b3
kjnam <kyujnam@gmail.com>
2018-12-09 21:29:02 +0900

    chore: profile에 따라 다른 설정값을 참조할 수 있도록 설정

--- src/test/java/io/iamkyu/controller/EventControllerTest.java
+++ src/test/java/io/iamkyu/controller/EventControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,6 +30,7 @@ import java.time.LocalDateTime;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class EventControllerTest {
 
     @Autowired
